fb78864bd4f2de19da05bbc88c9f81b4023517fb
Julien RICHARD <julien.y.richard@gmail.com>
2024-09-26 15:54:34 +0200

    Mise en place de la route Delete, bonus sur les Query params

--- api/src/repos/repos.controllers.ts
+++ api/src/repos/repos.controllers.ts
@@ -4,6 +4,7 @@ import Joi from "joi";
 import repos from "../../data/repos.json";
 import type { Repo } from "./repo.type";
 
+let myRepos: Array<Repo> = repos;
 
 const repoControllers = express.Router();
 
@@ -24,12 +25,14 @@ const validateRepo = (req: Request, res: Response, next: NextFunction) => {
   }
 }
 
-repoControllers.get('/', (_: any, res: Response) => {
-  res.status(200).json(repos)
+repoControllers.get('/', (req: Request, res: Response) => {
+  const { status } = req.query;
+  const result = status !== undefined ? myRepos.filter((repo: Repo) => repo.isPrivate === +status) : myRepos;
+  res.status(200).json(result)
 })
 
 repoControllers.get('/:id', (req: Request, res: Response) => {
-  const repo = repos.find(rep => rep.id === req.params.id) as Repo;
+  const repo = myRepos.find(rep => rep.id === req.params.id) as Repo;
 
   if (repo) {
     res.status(200).json(repo)
@@ -39,9 +42,14 @@ repoControllers.get('/:id', (req: Request, res: Response) => {
 })
 
 repoControllers.post('/', validateRepo, (req: Request, res: Response) => {
-  repos.push(req.body)
+  myRepos.push(req.body)
   res.status(201).json(req.body)
 })// http://localhost:3001/api/repos/ <=> http://localhost:3001/users/:id/repos/
+
+repoControllers.delete('/:id', (req: Request, res: Response) => {
+  myRepos = myRepos.filter((repo: Repo) => repo.id !== req.params.id)
+  res.sendStatus(204)
+})
 
 
 export default repoControllers;
