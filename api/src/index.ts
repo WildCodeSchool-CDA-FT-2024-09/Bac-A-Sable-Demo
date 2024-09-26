8b10fdc5cc3c05d9058dc5d9795ccd85fa130504
Julien RICHARD <julien.y.richard@gmail.com>
2024-09-26 10:29:40 +0200

    Mise en place de la route POST et validation de données

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,8 +3,11 @@ import router from "./router"
 const app = express();
 
 // app.get('/maroute, fonction de callback)
+app.use(express.json());
 
-app.use('/api', router)
+
+
+app.use('/api', router);
 
 app.listen(3001, () => {
   console.log(`Serveur is listenning on http://localhost:3001`);
--- api/src/repos/repos.controllers.ts
+++ api/src/repos/repos.controllers.ts
@@ -1,8 +1,28 @@
-import express, { Response, Request } from "express";
+import express, { Response, Request, NextFunction } from "express";
+import Joi from "joi";
+
 import repos from "../../data/repos.json";
-import type { Repo } from "./repo.type"
+import type { Repo } from "./repo.type";
+
 
 const repoControllers = express.Router();
+
+const schema = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+  url: Joi.string().required(),
+  isPrivate: Joi.number().min(1).max(2).required()
+})
+
+const validateRepo = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body)
+
+  if (error == null) {
+    next()
+  } else {
+    res.status(422).json(error)
+  }
+}
 
 repoControllers.get('/', (_: any, res: Response) => {
   res.status(200).json(repos)
@@ -17,5 +37,11 @@ repoControllers.get('/:id', (req: Request, res: Response) => {
     res.sendStatus(404)
   }
 })
+
+repoControllers.post('/', validateRepo, (req: Request, res: Response) => {
+  repos.push(req.body)
+  res.status(201).json(req.body)
+})// http://localhost:3001/api/repos/ <=> http://localhost:3001/users/:id/repos/
+
 
 export default repoControllers;
